c56d7e11836a56f02d62f9f7662fca49f1417100
Michael Rommel <michamaxe@gmail.com>
2020-07-23 16:26:11 +0200

    cleanup policy card

--- SmartColony/Nodes/PolicyCardNode.swift
+++ SmartColony/Nodes/PolicyCardNode.swift
@@ -16,88 +16,63 @@ class PolicyCardNode: SKNode {
     var nameLabel: SKLabelNode?
     //var shadowNameLabel: SKLabelNode?
     var bonusLabel: SKLabelNode?
-    
+
     // MARK: constructors
-    
+
     init(policyCard: PolicyCardType) {
-        
+
         super.init()
-        
+
         let size = CGSize(width: 100, height: 100)
-        
+
         let texture = SKTexture(imageNamed: policyCard.iconTexture())
         self.backgroundNode = SKSpriteNode(texture: texture, color: .black, size: size)
         self.backgroundNode?.position = CGPoint(x: size.halfWidth, y: -size.halfHeight)
         self.addChild(self.backgroundNode!)
+        
+        let titleTextAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .backgroundColor: UIColor.black.withAlphaComponent(0.7),
+            .font: UIFont.boldSystemFont(ofSize: 12)
+        ]
 
-        self.nameLabel = SKLabelNode(text: policyCard.name())
+        let titleTextAttributed = NSAttributedString(string: policyCard.name(), attributes: titleTextAttributes)
+
+        self.nameLabel = SKLabelNode(attributedText: titleTextAttributed)
         self.nameLabel?.fontSize = 12
         self.nameLabel?.fontColor = .black
         self.nameLabel?.zPosition = self.zPosition + 10
         self.nameLabel?.verticalAlignmentMode = .center
-        self.nameLabel?.position = CGPoint(x: size.halfWidth, y: -20)
+        self.nameLabel?.position = CGPoint(x: size.halfWidth, y: -15)
         self.addChild(self.nameLabel!)
-        
-        let tmpText = policyCard.bonus()
-        let bonusText: NSMutableAttributedString
-        do {
-            let regex = try NSRegularExpression(pattern: "(Civ6Production|Civ6Gold|Civ6StrengthIcon)", options: NSRegularExpression.Options())
-            let splits = regex.split(tmpText)
 
-            let attText = NSMutableAttributedString()
-            let attributes: [NSAttributedString.Key: Any] = [
-                .foregroundColor: UIColor.white,
-                .backgroundColor: UIColor.red,
-                .font: UIFont.boldSystemFont(ofSize: 10)
-            ]
-            
-            for split in splits {
-                
-                if split == "Civ6StrengthIcon" {
-                    let attachment = NSTextAttachment()
-                    attachment.image = UIImage(named: "StrengthIcon")!.resize(to: CGSize(width: 32, height: 32))
-                    attachment.bounds = CGRect(x: 0, y: 0, width: 32, height: 32)
-                    attText.append(NSAttributedString(attachment: attachment))
-                } else {
-                    attText.append(NSAttributedString(string: split, attributes: attributes))
-                }
-            }
-            
-            bonusText = attText
-            
-        } catch {
-            print("failed")
-            bonusText = NSMutableAttributedString(string: policyCard.bonus())
-        }
+        var tmpText = policyCard.bonus()
         
-        self.bonusLabel = SKLabelNode(attributedText: bonusText)
-        self.bonusLabel?.fontSize = 8
-        self.bonusLabel?.fontColor = .black
+        tmpText = tmpText.replacingOccurrences(of: "Civ6StrengthIcon", with: "🛡")
+        // Civ6Production ⚙
+        // Civ6Gold 🪙
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        let bonusTextAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .backgroundColor: UIColor.black.withAlphaComponent(0.7),
+            .font: UIFont.boldSystemFont(ofSize: 10),
+            .paragraphStyle: paragraphStyle
+        ]
+
+        let bonusTextAttributed = NSAttributedString(string: tmpText, attributes: bonusTextAttributes)
+        
+        self.bonusLabel = SKLabelNode(attributedText: bonusTextAttributed)
         self.bonusLabel?.zPosition = self.zPosition + 10
-        self.bonusLabel?.verticalAlignmentMode = .center
+        self.bonusLabel?.verticalAlignmentMode = .top
         self.bonusLabel?.numberOfLines = 0
         self.bonusLabel?.preferredMaxLayoutWidth = 60
-        self.bonusLabel?.position = CGPoint(x: size.halfWidth, y: -60)
-        
-        if let attributedText = self.bonusLabel?.attributedText {
-          attributedText.enumerateAttribute(NSAttributedString.Key.attachment, in: NSRange(location: 0, length: attributedText.length), options: []) { (value, range, stop) in
-                if value is NSTextAttachment {
-                    if let attachment = value as? NSTextAttachment {
-                        
-                        if let image = attachment.image {
-                            let texture = SKTexture(image: image)
-                            let imageSpriteNode = SKSpriteNode(texture: texture)
-                            print("attachment.bounds: \(attachment.bounds)")
-                            self.bonusLabel?.addChild(imageSpriteNode)
-                        }
-                    }
-                }
-            }
-        }
-        
+        self.bonusLabel?.position = CGPoint(x: size.halfWidth, y: -22)
+
         self.addChild(self.bonusLabel!)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
