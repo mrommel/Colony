bb005fd65dae3f4be789e991f0ab7287c539c6fc
Michael Rommel <michamaxe@gmail.com>
2021-08-11 11:50:13 +0200

    Wonders can be build when already built #68 - also add a built wonder to the game wonder list - add test

--- SmartAILibraryTests/Helpers/TestUI.swift
+++ SmartAILibraryTests/Helpers/TestUI.swift
@@ -18,7 +18,7 @@ class TestUI: UserInterfaceDelegate {
     
     func update(city: AbstractCity?) { }
 
-    func showPopup(popupType: PopupType, with data: PopupData?) { }
+    func showPopup(popupType: PopupType) { }
 
     func showScreen(screenType: ScreenType, city: AbstractCity?) { }
     
