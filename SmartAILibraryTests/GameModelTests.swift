5645d27e33581ec1e53a63b0a7a8a44e302d96e7
Michael Rommel <michamaxe@gmail.com>
2021-08-11 12:20:26 +0200

    rebase

--- SmartAILibraryTests/GameModelTests.swift
+++ SmartAILibraryTests/GameModelTests.swift
@@ -84,4 +84,91 @@ class GameModelTests: XCTestCase {
         // THEN
         XCTAssertEqual(worldEra, .medieval)
     }
+    
+    func testWonderCantBeBuiltTwice() {
+        // GIVEN
+     
+        // player 1
+        let playerAlexander = Player(leader: .alexander, isHuman: true)
+        playerAlexander.initialize()
+        playerAlexander.set(era: .classical)
+        try! playerAlexander.techs?.discover(tech: .masonry)
+        
+        // player 2
+        let playerTrajan = Player(leader: .trajan)
+        playerTrajan.initialize()
+        playerTrajan.set(era: .medieval)
+        
+        // map
+        let mapModel = MapModelHelper.mapFilled(with: .grass, sized: .duel)
+
+        // game
+        let gameModel = GameModel(victoryTypes: [.domination, .cultural, .diplomatic],
+                                  handicap: .chieftain,
+                                  turnsElapsed: 0,
+                                  players: [playerAlexander, playerTrajan],
+                                  on: mapModel)
+        
+        // city
+        let city = City(name: "Berlin", at: HexPoint.zero, owner: playerAlexander)
+        city.initialize(in: gameModel)
+        gameModel.add(city: city)
+        let canBuildBefore = city.canBuild(wonder: .pyramids, in: gameModel)
+        
+        // WHEN
+        city.startBuilding(wonder: .pyramids)
+        city.updateProduction(for: 2000, in: gameModel)
+        let canBuildAfter = city.canBuild(wonder: .pyramids, in: gameModel)
+        
+        // THEN
+        XCTAssertEqual(canBuildBefore, true)
+        XCTAssertEqual(canBuildAfter, false)
+    }
+    
+    // https://github.com/mrommel/Colony/issues/68
+    func testWonderCantBeBuiltWhenBuiltByAnotherPlayer() {
+        // GIVEN
+     
+        // player 1
+        let playerAlexander = Player(leader: .alexander, isHuman: true)
+        playerAlexander.initialize()
+        playerAlexander.set(era: .classical)
+        try! playerAlexander.techs?.discover(tech: .masonry)
+        
+        // player 2
+        let playerTrajan = Player(leader: .trajan)
+        playerTrajan.initialize()
+        playerTrajan.set(era: .classical)
+        try! playerTrajan.techs?.discover(tech: .masonry)
+        
+        // map
+        let mapModel = MapModelHelper.mapFilled(with: .grass, sized: .duel)
+
+        // game
+        let gameModel = GameModel(victoryTypes: [.domination, .cultural, .diplomatic],
+                                  handicap: .chieftain,
+                                  turnsElapsed: 0,
+                                  players: [playerAlexander, playerTrajan],
+                                  on: mapModel)
+        
+        // city 1
+        let city1 = City(name: "Berlin", at: HexPoint.zero, owner: playerAlexander)
+        city1.initialize(in: gameModel)
+        gameModel.add(city: city1)
+        
+        // city 2
+        let city2 = City(name: "Potsdam", at: HexPoint(x: 20, y: 20), owner: playerTrajan)
+        city2.initialize(in: gameModel)
+        gameModel.add(city: city2)
+        let canBuildBefore = city2.canBuild(wonder: .pyramids, in: gameModel)
+        
+        // WHEN
+        city1.startBuilding(wonder: .pyramids)
+        city1.updateProduction(for: 2000, in: gameModel)
+        let canBuildAfter = city2.canBuild(wonder: .pyramids, in: gameModel)
+        
+        // THEN
+        XCTAssertEqual(canBuildBefore, true)
+        XCTAssertEqual(canBuildAfter, false)
+    }
 }
