412bb6ed880c9ddd5197cf5dd6145ee56e4cd815
Michael Rommel <michamaxe@gmail.com>
2022-01-20 14:20:59 +0100

    refactor tooltip

--- SmartColonyMacOSUILibrary/SmartColonyMacOSUILibrary/Views/Scenes/Nodes/Layers/Objects/TooltipNode.swift
+++ SmartColonyMacOSUILibrary/SmartColonyMacOSUILibrary/Views/Scenes/Nodes/Layers/Objects/TooltipNode.swift
@@ -35,6 +35,15 @@ extension NSAttributedString {
     }
 }
 
+extension String {
+
+  func boundingRect(with size: CGSize, attributes: [NSAttributedString.Key: Any]) -> CGRect {
+      let options: NSString.DrawingOptions = [.usesLineFragmentOrigin, .usesFontLeading]
+      let rect = self.boundingRect(with: size, options: options, attributes: attributes, context: nil)
+      return rect
+  }
+}
+
 // https://stackoverflow.com/questions/57263134/sklabelnode-possible-to-insert-nstextattachment-or-icon-into-block-of-text
 class AdvancedLabelNode: SKLabelNode {
 
@@ -42,25 +51,43 @@ class AdvancedLabelNode: SKLabelNode {
         super.init()
         self.attributedText = attributedText
 
+        var lengths: [CGFloat] = []
+        var iconIndices: [Int] = []
+        var iconIndex: Int = 0
         let completeRange = NSRange(location: 0, length: attributedText.length)
+
         attributedText.enumerateAttributes(in: completeRange, options: []) { (value, range, _) in
 
-            print("value: \(value), range = \(range)")
-            let part = attributedText.string.substring(with: Range(range, in: attributedText.string)!)
-            print("part: '\(part)'")
+            let part: String = String(attributedText.string[Range(range, in: attributedText.string)!])
+            let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: 20.0)
+            let size = part.boundingRect(with: constraintRect, attributes: value)
+            lengths.append(size.width)
+
+            if value[NSAttributedString.Key.attachment] != nil {
+                iconIndices.append(iconIndex)
+            }
+
+            iconIndex += 1
         }
 
-        attributedText.enumerateAttribute(NSAttributedString.Key.attachment, in: completeRange, options: []) { (value, range, stop) in
+        for index in 1..<lengths.count {
+            lengths[index] = lengths[index - 1] + lengths[index]
+        }
+
+        iconIndex = 0 // reset
+
+        attributedText.enumerateAttribute(NSAttributedString.Key.attachment, in: completeRange, options: []) { (value, _, _) in
 
             if let attachment = value as? NSTextAttachment, let image = attachment.image {
 
-                print("image bounds = \(attachment.bounds), range = \(range), stop = \(stop)")
                 let texture = SKTexture(image: image)
                 let imageSpriteNode = SKSpriteNode(texture: texture)
                 imageSpriteNode.size = attachment.bounds.size
-                imageSpriteNode.position = CGPoint(x: CGFloat(range.location) * 5.6 - (self.frame.width / 2), y: 0)
+                imageSpriteNode.position = CGPoint(x: lengths[iconIndices[iconIndex]] - 6 - (self.frame.width / 2), y: 0)
                 imageSpriteNode.zPosition = self.zPosition + 0.01
                 self.addChild(imageSpriteNode)
+
+                iconIndex += 1
             }
         }
     }
@@ -68,12 +95,6 @@ class AdvancedLabelNode: SKLabelNode {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    /*override var zPosition: CGFloat {
-        set {
-
-        }
-    }*/
 }
 
 class TooltipNode: SKNode {
