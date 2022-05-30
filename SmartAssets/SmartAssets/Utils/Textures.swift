980d226c4fecdcb34ecc8c197c61044bad67600e
Michael Rommel <michamaxe@gmail.com>
2022-05-30 11:20:12 +0200

    mountain caldera overlaps natural wonders #239

--- SmartAssets/SmartAssets/Utils/Textures.swift
+++ SmartAssets/SmartAssets/Utils/Textures.swift
@@ -198,6 +198,10 @@ public class Textures {
             return true
         }
 
+        if feature == .mountEverest || feature == .mountKilimanjaro {
+            return true
+        }
+
         return false
     }
 
