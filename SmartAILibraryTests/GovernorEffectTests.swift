370069737ac906c922d43ed532373fc4591da38b
Michael Rommel <michamaxe@gmail.com>
2022-07-07 22:05:59 +0200

    prepare more governor effect tests

--- SmartAILibraryTests/GovernorEffectTests.swift
+++ SmartAILibraryTests/GovernorEffectTests.swift
@@ -215,7 +215,7 @@ class GovernorEffectTests: XCTestCase {
 
     // redoubt
     // - Increase city garrison [Strength] Combat Strength by 5.
-    func testGovernorReynaRedoubt() throws {
+    func testGovernorVictorRedoubt() throws {
 
         // GIVEN
         let cityStrengthBefore = self.objectToTest?.defensiveStrength(against: nil, on: nil, ranged: false, in: self.gameModel)
@@ -235,7 +235,7 @@ class GovernorEffectTests: XCTestCase {
     // garrisonCommander
     // - Units defending within the city's territory get +5 [Strength] Combat Strength.
     // - Your other cities within 9 tiles gain +4 Loyalty per turn towards your civilization.
-    func testGovernorReynaGarrisonCommander() throws {
+    func testGovernorVictorGarrisonCommander() throws {
 
         // GIVEN
         let secondCity = City(name: "Potsdam", at: HexPoint(x: 6, y: 6), owner: self.playerAlexander)
@@ -254,7 +254,7 @@ class GovernorEffectTests: XCTestCase {
         }
 
         let defenseModifierBefore = warriorBefore?.defenseModifier(against: nil, or: nil, on: centerTile, ranged: false, in: self.gameModel)
-        let loyaltyBefore = secondCity.loyalty()
+        let loyaltyBefore = secondCity.loyaltyFromGovernors(in: self.gameModel)
 
         // WHEN
         self.playerAlexander?.governors?.addTitle()
@@ -269,19 +269,19 @@ class GovernorEffectTests: XCTestCase {
         }
 
         let defenseModifierAfter = warriorAfter?.defenseModifier(against: nil, or: nil, on: centerTile, ranged: false, in: self.gameModel)
-        let loyaltyAfter = secondCity.loyalty()
+        let loyaltyAfter = secondCity.loyaltyFromGovernors(in: self.gameModel)
 
         // THEN
         XCTAssertEqual(defenseModifierBefore, 2)
         XCTAssertEqual(defenseModifierAfter, 7)
-        XCTAssertEqual(loyaltyBefore, 100)
-        XCTAssertEqual(loyaltyAfter, 100)
+        XCTAssertEqual(loyaltyBefore, 0)
+        XCTAssertEqual(loyaltyAfter, 4)
     }
 
     // defenseLogistics
     // - City cannot be put under siege. // #
     // - Accumulating Strategic resources gain an additional +1 per turn.
-    func testGovernorReynaDefenseLogistics() throws {
+    func testGovernorVictorDefenseLogistics() throws {
 
         // GIVEN
         // WHEN
@@ -291,7 +291,7 @@ class GovernorEffectTests: XCTestCase {
     // embrasure
     // - City gains an additional Ranged Strike per turn.
     // - Military units trained in this city start with a free promotion that do not already start with a free promotion.
-    func testGovernorReynaEmbrasure() throws {
+    func testGovernorVictorEmbrasure() throws {
 
         // GIVEN
         let cityAttacksBefore = self.objectToTest?.numberOfAttacksPerTurn(in: self.gameModel)
@@ -320,7 +320,7 @@ class GovernorEffectTests: XCTestCase {
 
     // airDefenseInitiative
     // - +25 [Strength] Combat Strength to anti-air support units within the city's territory when defending against aircraft and ICBMs. // #
-    func testGovernorReynaAirDefenseInitiative() throws {
+    func testGovernorVictorAirDefenseInitiative() throws {
 
         // GIVEN
         // WHEN
@@ -329,10 +329,172 @@ class GovernorEffectTests: XCTestCase {
 
     // armsRaceProponent
     // - 30% [Production] Production increase to all nuclear armament projects in the city. // #
-    func testGovernorReynaArmsRaceProponent() throws {
+    func testGovernorVictorArmsRaceProponent() throws {
 
         // GIVEN
         // WHEN
         // THEN
     }
+
+    // MARK: governor Amani tests
+
+    // messenger
+    // - Can be assigned to a City-state, where she acts as 2 [Envoy] Envoys. // #
+    func testGovernorAmaniMessenger() throws {
+
+        // GIVEN
+        // WHEN
+        // THEN
+    }
+
+    // emissary
+    // - Other cities within 9 tiles and not owned by you gain +2 Loyalty per turn towards your civilization.
+    func testGovernorAmaniEmissary() throws {
+
+        // GIVEN
+        let trajanCity = City(name: "Cottbus", at: HexPoint(x: 6, y: 4), owner: self.playerTrajan)
+        trajanCity.initialize(in: self.gameModel)
+        self.gameModel?.add(city: trajanCity)
+
+        let loyaltyBefore = trajanCity.loyaltyFromGovernors(in: self.gameModel)
+
+        // WHEN
+        self.playerAlexander?.governors?.addTitle()
+        self.playerAlexander?.governors?.appoint(governor: .amani, in: self.gameModel)
+        self.objectToTest?.assign(governor: .amani)
+        self.objectToTest?.governor()?.promote(with: .emissary)
+
+        let loyaltyAfter = trajanCity.loyaltyFromGovernors(in: self.gameModel)
+
+        // THEN
+        XCTAssertEqual(loyaltyBefore, 0)
+        XCTAssertEqual(loyaltyAfter, -2)
+    }
+
+    // affluence
+    // - While established in a city-state, provides a copy of its Luxury resources to you. // #
+    func testGovernorAmaniAffluence() throws {
+
+        // GIVEN
+        // WHEN
+        // THEN
+    }
+
+    // localInformants
+    // Enemy Spies operate at 3 levels below normal in this city. // #
+    func testGovernorAmaniLocalInformants() throws {
+
+        // GIVEN
+        // WHEN
+        // THEN
+    }
+
+    // foreignInvestor
+    // - While established in a city-state, accumulate its Strategic resources. // #
+    // - When suzerain receive double the amount of accumulated strategic resources. // #
+    func testGovernorAmaniForeignInvestor() throws {
+
+        // GIVEN
+        // WHEN
+        // THEN
+    }
+
+    // puppeteer
+    // - While established in a city-state, doubles the number of [Envoy] Envoys you have there. // #
+    func testGovernorAmaniPuppeteer() throws {
+
+        // GIVEN
+        // WHEN
+        // THEN
+    }
+
+    // MARK: governor Magnus tests
+
+    // groundbreaker
+    // - +50% yields from plot harvests and feature removals in the city. // #
+
+    // surplusLogistics
+    // - +20% [Food] Food Growth in the city.
+    // - Your [TradeRoute] Trade Routes ending here provide +2 [Food] Food to their starting city.
+
+    // provision
+    // - Settlers trained in the city do not consume a [Citizen] Citizen Population.
+
+    // industrialist
+    // - Increase the Power provided by each resource of the Coal Power Plant, Oil Power Plant and Nuclear Power Plant by 1 and the [Production] Production by 2. // #
+
+    // blackMarketeer
+    // - Strategic resources for units are discounted 80%.
+
+    // verticalIntegration
+    // - This city receives [Production] Production from any number of Industrial Zones within 6 tiles, not just the first. // #
+
+    // MARK: governor Moksha tests
+
+    // bishop
+    // - Religious pressure to adjacent cities is 100% stronger from this city. // #
+    // - +2 [Faith] Faith per specialty district in this city.
+
+    // grandInquisitor
+    // - +10 [ReligiousStrength] Religious Strength in theological combat in tiles of this city. // #
+
+    // layingOnOfHands
+    // - All Governor's units heal fully in one turn in tiles of this city.
+
+    // citadelOfGod
+    // - City ignores pressure and combat effects from Religions not founded by the Governor's player. // #
+    // - Gain [Faith] Faith equal to 25% of the construction cost when finishing buildings. // #
+
+    // patronSaint
+    // - Apostles and Warrior Monks trained in the city receive 1 extra Promotion when receiving their first promotion. // #
+
+    // divineArchitect
+    // - Allows city to purchase Districts with [Faith] Faith.
+
+    // MARK: governor Liang tests
+
+    // guildmaster
+    // - All Builders trained in city get +1 build charge.
+
+    // zoningCommissioner
+    // - +20% [Production] Production towards constructing Districts in the city.
+
+    // aquaculture
+    // - The Fishery unique improvement can be built in the city on coastal plots. // #
+    // - Yields 1 [Food] Food, +1 [Food] Food for each adjacent sea resource. // #
+    // - Fisheries provide +1 [Production] Production if Liang is in the city. // #
+
+    // reinforcedMaterials
+    // - This city's improvements, buildings and Districts cannot be damaged by Environmental Effects. // #
+
+    // waterWorks
+    // - +2 [Housing] Housing for every Neighborhood and Aqueduct district in this city.
+    // - +1 [Amenity] Amenity for every Canal and Dam district in this city. // #
+
+    // parksAndRecreation:
+    // - The City Park unique improvement can be built in the city. // #
+    // - Yields 2 Appeal and 1 [Culture] Culture. // #
+    // - +1 [Amenity] Amenity if adjacent to water. // #
+    // - City Parks provide 3 [Culture] Culture if Liang is in the city. // #
+
+
+    // MARK: governor Pingala tests
+
+    // librarian
+    // - 15% increase in [Science] Science and [Culture] Culture generated by the city.
+
+    // connoisseur
+    // - +1 [Culture] Culture per turn for each Citizen in the city.
+
+    // researcher
+    // - +1 [Science] Science per turn for each Citizen in the city.
+
+    // grants
+    // - +100% [GreatPeople] Great People points generated per turn in the city.
+
+    // spaceInitiative
+    // - 30% [Production] Production increase to all space-program projects in the city. // #
+
+    // curator
+    // - +100% [Tourism] Tourism from Great Works of Art, Music, and Writing in the city. // #
 }
